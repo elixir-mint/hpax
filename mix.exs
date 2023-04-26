4699d88686dc81d2c3f2f78a8eaf8b45d839ac72
Andrea Leopardi <an.leopardi@gmail.com>
2023-04-26 19:33:01 +0200

    Bump Elixir requirement to 1.7+ (#14)

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule HPAX.MixProject do
     [
       app: :hpax,
       version: @version,
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
